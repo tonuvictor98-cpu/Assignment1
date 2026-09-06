5885fd25caad9c2c8fbb8ca9c41a10c46deb5553
tonuvictor98-cpu <tonuvictor98@gmail.com>
2026-09-06 10:39:58 +0200

    ughhhhhhhh

---
+++ Server/InMemoryRepositories/UserInMemoryRepository.cs
@@ -0,0 +1,63 @@
+using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class UserInMemoryRepository : IUserRepository
+{
+    private readonly List<User> users = new List<User>();
+
+    public Task<User> AddAsync(User user)
+    {
+        user.Id = users.Any()
+            ? users.Max(x => x.Id) + 1
+            : 1;
+        users.Add(user);
+        return Task.FromResult(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        User? existingUser = users.SingleOrDefault(x => x.Id == user.Id);
+        if (existingUser is null)
+        {
+            throw new InvalidOperationException(
+                $"User with ID '{user.Id}' not found");
+        }
+
+        users.Remove(existingUser);
+        users.Add(user);
+
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        User? userToRemove = users.SingleOrDefault(x => x.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"User with ID '{id}' not found");
+        }
+
+        users.Remove(userToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<User> GetSingleAsync(int id)
+    {
+        User? user = users.SingleOrDefault(x => x.Id == id);
+        if (user is null)
+        {
+            throw new InvalidOperationException(
+                $"User with ID '{id}' not found");
+        }
+
+        return Task.FromResult(user);
+    }
+
+    public IQueryable<User> GetMany()
+    {
+        return users.AsQueryable();
+    }
+}
